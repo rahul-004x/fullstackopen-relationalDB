915fbe9e1c9e7f931cecfc8db1be6341629a229c
Rahul <rahul@Mac.lan>
2025-03-10 20:14:42 +0530

    support operations for GET, POST and DELETE

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, DataTypes, Model } = require('sequelize')
 const express = require('express')
 const app = express()
+
+// Add this middleware to parse JSON request bodies
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
@@ -19,12 +22,54 @@ const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 // main()
 
-app.get('/', async (req, res) => {
+class Blog extends Model {}
+Blog.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    likes: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blogs'  
+})
+
+app.get('/api/blogs', async (req, res) => {
   const blogs =  await sequelize.query('SELECT * FROM blogs', { type: sequelize.QueryTypes.SELECT })
-  res.json(blogs) 
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+    console.log(req.body)
+    const blogs = await Blog.create(req.body)
+    res.json(blogs)
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+    const blog = await Blog.findByPk(req.params.id)
+    await blog.destroy()
+    res.status(204).end()
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-}) 
+})
